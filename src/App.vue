9c7d46a3cec88dc72cb8c7ae17b8650e47c36c78
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-14 16:38:57 +0530

    new component created and used props

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
-<template lang="">
+<template>
   <main>
+
     <!-- heading -->
     <header>
-      <img src="./assets/pinia-logo.svg" alt="pinia-logo">
+      <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <!-- <p>{{ task.title }}</p> -->
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
   </main>
 </template>
+
 <script>
+import TaskDetails from './components/TaskDetails.vue'
+
+import { useTaskStore } from './stores/TaskStore'
+
 export default {
-  
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore()
+
+    return { taskStore }
+  }
 }
 </script>
-<style lang="">
-  
-</style>
-
