bd4e2712c94ced3ed37b887e9d65f398c4fdd366
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-14 15:13:28 +0530

    npm install pinia

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia-logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
+
