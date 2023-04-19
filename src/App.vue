e5d5aa8c569108fd9bc022d435abc4586cf62060
Abhinand K <abhinandkofficial01@gmail.com>
2023-04-19 16:09:57 +0530

    new component created

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
     <!-- filter -->
     <nav class="filter">
       <button @click="filter = 'all'">All Tasks</button>
@@ -32,12 +37,12 @@
 
 <script>
 import { ref } from 'vue'
-import TaskDetails from './components/TaskDetails.vue'
-
-import { useTaskStore } from './stores/TaskStore'
+import TaskDetails from './components/TaskDetails.vue';
+import TaskForm from './components/TaskForm.vue';
+import { useTaskStore } from './stores/TaskStore';
 
 export default {
-  components: { TaskDetails },
+  components: { TaskDetails, TaskForm },
   setup() {
     const taskStore = useTaskStore()
 
